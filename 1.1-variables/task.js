b7a25900a7ce6620eae651bc428adae8d6a231b7
deizee <deizee@mail.ru>
2020-09-30 14:41:36 +0500

    Доработка домашнего задания 1.1

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -19,23 +19,18 @@ function averageMark(a, g, p) {
 function sayHello(userName) {
 	
 	let myName = userName;
-	let message = 'Привет, мир! Меня зовут ' + myName;
+	let message = `Привет, мир! Меня зовут ${myName}`;
     
 	return message;
 }
 
 function calculateFormula() {
     
-	let x;
-	let y;
-	let z;
-
-	x = 2;
-	y = 22;
-	z = 0;
+	let x = 2;
+	let y = 22;
+	let z = 0;
 
 	let result = x * y + 5 * z + x - 1;
 
-    
     return result;
 }

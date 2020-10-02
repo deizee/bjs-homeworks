2f20e275651493db341b7378e28c652ff2edcdf0
deizee <deizee@mail.ru>
2020-10-02 20:13:40 +0500

    ДЗ 1.2 "Расширенный синтаксис"

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -3,24 +3,24 @@
 function getResult(a,b,c){
     let d = Math.pow(b, 2) - 4 * a * c;
     let x = [];
+
     if (d > 0) {
         x = [(-b + Math.sqrt(d)) / 2 * a, (-b - Math.sqrt(d)) / 2 * a]
-        return x;
     } else if (d === 0) {
         x = [(-b + Math.sqrt(d)) / 2 * a];
-        return x;
-    } else return x;
+    }
+
+    return x;
 }
 
 function getAverageMark(marks){
-    let averageMark;
     let numberOfMarks = marks.length;
     let sumOfMarks = 0;
 
     if (numberOfMarks === 0) {
         return 0;
     } else if (numberOfMarks > 5) {
-        alert("Количество оценок больше пяти. Будут считаться первые пять");
+        console.log("Количество оценок больше пяти. Будут считаться первые пять");
         marks = marks.slice(0, 5);
         numberOfMarks = 5;
     }
@@ -29,9 +29,7 @@ function getAverageMark(marks){
         sumOfMarks += mark; 
     }
 
-    averageMark = sumOfMarks / numberOfMarks;
-
-    return averageMark;
+    return sumOfMarks / numberOfMarks;
 }
 
 function askDrink(name,dateOfBirthday){

b1b3849768d0b6d67f1e34d9f9d51d7b62323d60
deizee <deizee@mail.ru>
2020-10-07 15:10:33 +0500

    ДЗ 1.3 "Типы данных"

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,36 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    
+    if (isNaN(percent)) {
+        return `Параметр "Процентная ставка" содержит неправильное значение "${window.percent.value}"`
+    } else if (isNaN(contribution)) {
+        return `Параметр "Начальный взнос" содержит неправильное значение "${window.contribution.value}"`
+    } else if  (isNaN(amount)) {
+        return `Параметр "Общая стоимость" содержит неправильное значение "${window.amount.value}"`
+    } 
+    
+    let today = new Date();
+    let todayMonth = today.getMonth();
+    let todayYear = today.getFullYear();
+    let dateMonth = date.getMonth();
+    let dateYear = date.getFullYear();
+    let creditBody = amount - contribution;   //тело кредита
+    let numberOfMonths = (dateYear - todayYear) * 12 - todayMonth + dateMonth;  //кол-во выплачиваемых месяцев
+    let interestRate = percent / 100 / 12    //процентная ставка
+    let monthlyFee = creditBody * (interestRate + interestRate / (((1 + interestRate) ** numberOfMonths) - 1));  //ежемесячный платеж
+    let totalAmount = numberOfMonths * monthlyFee;  // общая сумма выплаты за кредит
+
+    console.log(Number(totalAmount.toFixed(2)));
+
+    return Number(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+
+    if (!name) {
+        return `Привет, мир! Меня зовут Аноним`
+    } else {
+        return `Привет, мир! Меня зовут ${name}`
+    }
 }

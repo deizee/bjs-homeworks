99a2821a121ca5cd4480ad32ce0f8243e99edbd8
deizee <deizee@mail.ru>
2020-10-08 09:51:05 +0500

    Доработака ДЗ 1.3 "Типы данных"

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -3,23 +3,23 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
     
     if (isNaN(percent)) {
-        return `Параметр "Процентная ставка" содержит неправильное значение "${window.percent.value}"`
+        return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`
     } else if (isNaN(contribution)) {
-        return `Параметр "Начальный взнос" содержит неправильное значение "${window.contribution.value}"`
+        return `Параметр "Начальный взнос" содержит неправильное значение ${contribution}`
     } else if  (isNaN(amount)) {
-        return `Параметр "Общая стоимость" содержит неправильное значение "${window.amount.value}"`
+        return `Параметр "Общая стоимость" содержит неправильное значение ${amount}`
     } 
     
-    let today = new Date();
-    let todayMonth = today.getMonth();
-    let todayYear = today.getFullYear();
-    let dateMonth = date.getMonth();
-    let dateYear = date.getFullYear();
-    let creditBody = amount - contribution;   //тело кредита
-    let numberOfMonths = (dateYear - todayYear) * 12 - todayMonth + dateMonth;  //кол-во выплачиваемых месяцев
-    let interestRate = percent / 100 / 12    //процентная ставка
-    let monthlyFee = creditBody * (interestRate + interestRate / (((1 + interestRate) ** numberOfMonths) - 1));  //ежемесячный платеж
-    let totalAmount = numberOfMonths * monthlyFee;  // общая сумма выплаты за кредит
+    const today = new Date();
+    const todayMonth = today.getMonth();
+    const todayYear = today.getFullYear();
+    const dateMonth = date.getMonth();
+    const dateYear = date.getFullYear();
+    const creditBody = amount - contribution;   //тело кредита
+    const numberOfMonths = (dateYear - todayYear) * 12 - todayMonth + dateMonth;  //кол-во выплачиваемых месяцев
+    const interestRate = percent / 100 / 12    //процентная ставка
+    const monthlyFee = creditBody * (interestRate + interestRate / (((1 + interestRate) ** numberOfMonths) - 1));  //ежемесячный платеж
+    const totalAmount = numberOfMonths * monthlyFee;  // общая сумма выплаты за кредит
 
     console.log(Number(totalAmount.toFixed(2)));
 
@@ -27,10 +27,5 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 }
 
 function getGreeting(name) {
-
-    if (!name) {
-        return `Привет, мир! Меня зовут Аноним`
-    } else {
-        return `Привет, мир! Меня зовут ${name}`
-    }
+    return `Привет, мир! Меня зовут ${(!name || name === 'null' || name === 'undefined' || name === '""' || name === "''") ? 'Аноним' : name}`
 }
